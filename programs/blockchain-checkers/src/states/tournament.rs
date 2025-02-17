edc75075e87e6595f33d8563a0b531af41b4a615
Mfoniso Ukpabio <regnalukpabio@gmail.com>
2025-02-17 02:46:09 +0100

    added Tournament account state

--- programs/blockchain-checkers/src/states/tournament.rs
+++ programs/blockchain-checkers/src/states/tournament.rs
@@ -1,6 +1,13 @@
 use anchor_lang::prelude::*;
 
+#[account]
+#[derive(InitSpace)]
 
-
-pub struct Tournament{
+pub struct Tournament {
+    pub seeds: u64,
+    pub host: Pubkey,
+    pub players: Vec<Pubkey>,
+    pub max_players: u8,
+    pub bump: u8,
+    pub total_price: u64,
 }
