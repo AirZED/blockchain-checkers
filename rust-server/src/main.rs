485dc5041a7b10f03d05f5532ef2afbc9711bdc0
Mfoniso Ukpabio <regnalukpabio@gmail.com>
2025-02-18 19:19:57 +0100

    putting a pause of the rust backend end

--- rust-server/src/main.rs
+++ rust-server/src/main.rs
@@ -1,16 +1,22 @@
 // https://github.com/Totodore/socketioxide/blob/main/examples/axum-echo-tls/axum_echo-tls.rs
 
 use std::{
+    collections::HashMap,
     net::{Ipv4Addr, SocketAddrV4},
+    sync::{Arc, Mutex},
     vec,
 };
 
 use axum::{http::HeaderValue, routing::get};
 use rand::Rng;
-use socketioxide::{extract::SocketRef, SocketIo};
+use socketioxide::{
+    extract::{Data, SocketRef},
+    handler::Value,
+    SocketIo,
+};
 use tower_http::cors::{AllowOrigin, Any, CorsLayer};
 
-use serde::Serialize;
+use serde::{de::value, Serialize};
 
 #[derive(Debug, Clone, Copy, Serialize, PartialEq)]
 enum PieceColor {
@@ -18,35 +24,35 @@ enum PieceColor {
     Black,
 }
 
-#[derive(Debug, Clone, Copy, Serialize)]
+#[derive(Debug, Clone, Copy, Serialize, PartialEq)]
 struct Piece {
     color: PieceColor,
     crowned: bool,
 }
 
-#[derive(Debug, Serialize, Clone)]
+#[derive(Debug, Clone, PartialEq)]
+struct GameRoom {
+    players: Players,
+    game_state: GameState,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct Players {
+    white: Option<String>,
+    black: Option<String>,
+}
+
+#[derive(Debug, Serialize, Clone, PartialEq, Copy)]
 struct GameState {
     board: [[Option<Piece>; 8]; 8],
     current_turn: PieceColor,
     move_count: u32,
 }
 
-#[derive(Debug, Clone)]
-struct GameRoom {
-    id: String,
-    players: Players,
-    game_state: GameState,
-}
-
-#[derive(Debug, Clone)]
-struct Players {
-    white: Option<String>,
-    black: Option<String>,
-}
-
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Coordinate(pub usize, pub usize);
 
+#[derive(Debug, Clone, Copy, PartialEq)]
 struct Move {
     from: Coordinate,
     to: Coordinate,
@@ -54,24 +60,29 @@ struct Move {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // let rooms = Vec::<GameRoom>::new();
+    let mut rooms = HashMap::<String, GameRoom>::new();
     let (layer, io) = SocketIo::new_layer();
 
     io.ns("/", |socket: SocketRef| {
         println!("Client connected: {:?}", socket.id);
-        let mut rooms: Vec<GameRoom> = vec![];
+
+        let rooms_inside = &mut rooms;
 
         socket.on("createRoom", move |socket: SocketRef| {
             let room_id = generate_room_id();
             let initial_state = create_initial_game_state();
 
-            rooms.push(GameRoom {
-                id: room_id.to_owned(),
-                players: Players {
-                    white: Some(socket.id.to_string()),
-                    black: None,
+            rooms.insert(
+                room_id.to_owned(),
+                GameRoom {
+                    players: Players {
+                        white: Some(socket.id.to_string()),
+                        black: None,
+                    },
+                    game_state: initial_state.clone(),
                 },
-                game_state: initial_state.clone(),
-            });
+            );
 
             socket.join(room_id.to_owned());
             socket
@@ -86,10 +97,47 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .ok();
         });
 
-        socket.on("joinRoom", move |roomId: &str| {
-            let room = rooms.iter().find(|room: &GameRoom| room.id == roomId);
-        });
+        socket.on(
+            "joinRoom",
+            move |socket: SocketRef, Data::<String>(room_id)| async move {
+                // let room = rooms.iter_mut().find(|room| room.id == room_id);
 
+                match rooms.get_mut(&room_id) {
+                    Some(room) => match &room.players.black {
+                        Some(_) => {
+                            socket.emit("error", "Room is already full").ok();
+                        }
+                        None => {
+                            room.players.black = Some(socket.id.as_str());
+                            socket.join([room_id.to_owned()]);
+
+                            socket
+                                .emit(
+                                    "gameJoined",
+                                    &serde_json::json!({
+                                        "roomId": room_id,
+                                        "playerColor": PieceColor::White,
+                                        "gameState": room.game_state
+                                    }),
+                                )
+                                .ok();
+
+                            io.to(room_id)
+                                .emit(
+                                    "gameStart",
+                                    &serde_json::json!({
+                            "gameState": room.game_state}),
+                                )
+                                .await
+                                .ok();
+                        }
+                    },
+                    None => {
+                        socket.emit("error", "Room not found").ok();
+                    }
+                }
+            },
+        );
         // Listen for "message" event
         socket.on("message", |socket: SocketRef| {
             socket.emit("message-back", "Hello World!").ok();
@@ -133,14 +181,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     fn generate_room_id() -> String {
-        let characters: Vec<char> =
-            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-                .chars()
-                .collect();
-        let mut random_number = rand::rng();
-
+        let mut rng = rand::rng();
+        const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
         (0..6)
-            .map(|_| characters[random_number.random_range(0..characters.len())])
+            .map(|_| {
+                let idx = rng.random_range(0..CHARSET.len());
+                CHARSET[idx] as char
+            })
             .collect()
     }
 
